9c7d952c07b13566ab17fc55eae2d96ab9e678f4
Iron Coal <Irondwarf1@gmail.com>
2012-04-29 16:23:34 +0200

    :q

--- Uebungen/2/A5.java
+++ Uebungen/2/A5.java
@@ -1,34 +1,34 @@
 class A5 {
-	public static void main(String[] args) {
-		if (args.length != 10) {
-			System.out.println("Aufruf mit 10 ganzen Zahlen");
-		} else {
-			int i, wert = 1;
-			int[] folge = new int[10];
-			for (i = 0; i < args.length; i++)
-				folge[i] = Integer.parseInt(args[i]);
-			System.out.println("Wert vor erstem Funktionsaufruf: " + wert);
-			pluseins(wert);
-			System.out.println("Wert nach erstem Funktionsaufruf: " + wert);
-			wert = maxsegsum(folge, args.length);
-			System.out.println("Maximale Segmentsumme: " + wert);
-		}
-	}
-	
-	static void pluseins(int a) {
-		a = a + 1;
-	}
-	static int maxsegsum(int b[], int n) {
-		int s = 0, h = 0, c = 0;
-		while (h != n) {
-			if (c + b[h] > 0)
-			c += b[h];
-			else 
-				c = 0;
-			if (c > s)
-				s = c;
-			h++;
-		}
-		return s;
-	}
+    public static void main(String[] args) {
+        if (args.length != 10) {
+            System.out.println("Aufruf mit 10 ganzen Zahlen");
+        } else {
+            int i, wert = 1;
+            int[] folge = new int[10];
+            for (i = 0; i < args.length; i++)
+                folge[i] = Integer.parseInt(args[i]);
+            System.out.println("Wert vor erstem Funktionsaufruf: " + wert);
+            pluseins(wert);
+            System.out.println("Wert nach erstem Funktionsaufruf: " + wert);
+            wert = maxsegsum(folge, args.length);
+            System.out.println("Maximale Segmentsumme: " + wert);
+        }
+    }
+    
+    static void pluseins(int a) {
+        a = a + 1;
+    }
+    static int maxsegsum(int b[], int n) {
+        int s = 0, h = 0, c = 0;
+        while (h != n) {
+            if (c + b[h] > 0)
+            c += b[h];
+            else 
+                c = 0;
+            if (c > s)
+                s = c;
+            h++;
+        }
+        return s;
+    }
 }
--- Uebungen/2/A7.java
+++ Uebungen/2/A7.java
@@ -1,40 +1,40 @@
 public class A7 {
 
-	public static void main(String[] args) {
-		String s;
-		s = copy(args);
-		System.out.println(s);
-		s = backwards(s);
-		System.out.println(s);
-		s = code(s);
-		System.out.println(s);
-	}
-	public static String copy(String[] liste) {
-		StringBuffer s = new StringBuffer();
-		for (int i = 0; i < liste.length; i++) {
-			s.append(liste[i]);
-			if (i < liste.length - 1)
-				s.append(" ");
-		}
-		return s.toString();
-	}
-	public static String backwards(String s) {
-		StringBuffer b = new StringBuffer(s);
-		b.reverse();
-		return b.toString();
-	}
-	public static String code(String s) {
-		StringBuffer b = new StringBuffer();
-		char c;
-		for (int i = 0; i < s.length(); i++) {
-			c = s.charAt(i);
-			if (Character.isLetter(c))
-				if (c == 'Z' || c == 'z')
-					c = (char) ((int) c - 25);
-				else
-					c++; 
-			b.append(c);	
-		}	
-		return b.toString();
-	}
+    public static void main(String[] args) {
+        String s;
+        s = copy(args);
+        System.out.println(s);
+        s = backwards(s);
+        System.out.println(s);
+        s = code(s);
+        System.out.println(s);
+    }
+    public static String copy(String[] liste) {
+        StringBuffer s = new StringBuffer();
+        for (int i = 0; i < liste.length; i++) {
+            s.append(liste[i]);
+            if (i < liste.length - 1)
+                s.append(" ");
+        }
+        return s.toString();
+    }
+    public static String backwards(String s) {
+        StringBuffer b = new StringBuffer(s);
+        b.reverse();
+        return b.toString();
+    }
+    public static String code(String s) {
+        StringBuffer b = new StringBuffer();
+        char c;
+        for (int i = 0; i < s.length(); i++) {
+            c = s.charAt(i);
+            if (Character.isLetter(c))
+                if (c == 'Z' || c == 'z')
+                    c = (char) ((int) c - 25);
+                else
+                    c++; 
+            b.append(c);    
+        }    
+        return b.toString();
+    }
 }
